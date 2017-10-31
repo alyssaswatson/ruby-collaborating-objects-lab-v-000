0eb52d83302144fa2be04e5390a517d2c38c8a14
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-31 18:45:27 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,13 +1,17 @@
 class MP3Importer
-  attr_accessor :path
+  attr_accessor :path, :files
 
   def initialize(path)
     @path = path
+    @files = []
   end
 
   def files
-    
-
+    Dir("./db/mp3s").each do |file|
+      file_array = files.split(" - ")
+      @files << file_array[1]
+    end
+    @files
   end
 
 end
