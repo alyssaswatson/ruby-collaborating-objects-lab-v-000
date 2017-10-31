8f8bd9cf1036885b13fbad6c23ed3133213609a9
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-31 19:06:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,9 +11,9 @@ class Song
     artists.add_song(self)
   end
 
-  def artist_name = (name)
+  def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
-    
+
 end
